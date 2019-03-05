5b38fa3f36fc85804a4316ec1c196c5bb9f8cea4
Orko Sinha <os92@cornell.edu>
2019-03-05 01:33:25 -0500

    Add documentation to data.py

--- src/data.py
+++ src/data.py
@@ -37,15 +37,24 @@ static_eateries = {}
 today = date.today()
 
 def start_update():
+  """Starts the data retrieval process.
+
+  Begins the process of getting data for the campus_eateries dictionary,
+  from Cornell Dining and the static source, and collegetown_eateries dictionary,
+  from Yelp.
+  """
   try:
     print('[{}] Updating campus'.format(datetime.now()))
+    # Get data for on-campus, Cornell-owned dining Options
     dining_query = requests.get(CORNELL_DINING_URL)
     data_json = dining_query.json()
     parse_eatery(data_json)
     merge_hours(campus_eateries)
+    # Get data for on-campus, 3rd-party dining options
     static_json = requests.get(STATIC_EATERIES_URL).json()
     parse_static_eateries(static_json)
     Data.update_data(campus_eateries)
+    # Get data for Collegetown eateries
     print('[{}] Updating collegetown'.format(datetime.now()))
     yelp_query = collegetown_search()
     parse_collegetown_eateries(yelp_query)
@@ -56,6 +65,13 @@ def start_update():
     Timer(UPDATE_DELAY, start_update).start()
 
 def parse_eatery(data_json):
+  """Parses a Cornell Dining json dictionary.
+
+  Fills the campus_eateries dictionary with objects of type CampusEateryType.
+
+  Args:
+      data_json (dict): a valid dictionary from the Cornell Dining json
+  """
   for eatery in data_json['data']['eateries']:
     eatery_id = eatery.get('id', resolve_id(eatery))
     dining_items = get_trillium_menu() if eatery_id == TRILLIUM_ID else parse_dining_items(eatery)
@@ -80,9 +96,20 @@ def parse_eatery(data_json):
     campus_eateries[new_eatery.id] = new_eatery
 
 def get_image_url(slug):
+  """Generates a URL for an image.
+
+  Creates a string that represents a url pointing towards the eatery image.
+
+  Args:
+      slug (string): a slugged name of an eatery
+  """
   return "{}{}.jpg".format(IMAGES_URL, slug)
 
 def parse_payment_methods(methods):
+  """Returns a PaymentMethodsType according to which payment methods are available at an eatery.
+  Args:
+      methods (json): a valid json segment for payments from Cornell Dining
+  """
   payment_methods = PaymentMethodsType()
   payment_methods.brbs = any(pay['descrshort'] == PAY_METHODS['brbs'] for pay in methods)
   payment_methods.cash = any(pay['descrshort'] == PAY_METHODS['credit'] for pay in methods)
@@ -93,12 +120,21 @@ def parse_payment_methods(methods):
   return payment_methods
 
 def parse_operating_hours(eatery, dining_items):
+  """Returns a list of OperatingHoursTypes for each dining event for an eatery.
+
+  Calls parse_events to populate the events field for OperatingHoursType.
+
+  Args:
+      eatery (dict): A valid json segment from Cornell Dining that contains eatery information
+      dining_items (list): A list that holds a dictionary for the items an eatery serves and a flag
+       for healthy options
+  """
   new_operating_hours = []
   hours_list = eatery['operatingHours']
   for hours in hours_list:
     new_date = hours.get('date', '')
     hours_events = hours['events']
-
+    #merge the dining items from the json (for non-dining hall eateries) with the menu for an event
     for event in hours_events:
       if not event['menu'] and dining_items:
         event['menu'] = dining_items
@@ -111,6 +147,15 @@ def parse_operating_hours(eatery, dining_items):
   return new_operating_hours
 
 def parse_events(event_list, event_date):
+  """Parses the events of an eatery.
+
+  Returns a list of EventType for each event of an eatery. Calls parse_food_stations to populate
+  the menu field for EventType.
+
+  Args:
+      event_list (list): The list of all events for an eatery
+      event_date (string): The date of all of the events of the eatery
+  """
   new_events = []
   for event in event_list:
     start, end = format_time(event.get('start', ''), event.get('end', ''), event_date)
@@ -125,6 +170,14 @@ def parse_events(event_list, event_date):
   return new_events
 
 def parse_food_stations(station_list):
+  """Parses the food stations available at an event
+
+  Returns a list containing FoodStationTypes for an eatery. Calls parse_food_items to populate
+  the items field for FoodStationType.
+
+  Args:
+      station_list (list): A list representing all of the stations in a dining hall
+  """
   new_stations = []
   if len(station_list) == 1 and not station_list[0]['items']:  # no menu actually provided
     return new_stations
@@ -137,6 +190,14 @@ def parse_food_stations(station_list):
   return new_stations
 
 def parse_food_items(item_list):
+  """Parses the food items available at a food station.
+
+  Returns a list containing FoodItemTypes for an eatery. This is the lowest level that
+  is parsed for a dining hall.
+
+  Args:
+      item_list (list): A list representing the individual food items for each station
+  """
   new_food_items = []
   for item in item_list:
     new_food_items.append(
@@ -148,6 +209,14 @@ def parse_food_items(item_list):
   return new_food_items
 
 def parse_dining_items(eatery):
+  """Parses the dining items of an eatery.
+
+  Returns a list that holds a dictionary for the items an eatery serves and a flag for healthy
+  options. Exclusive to non-dining hall eateries.
+
+  Args:
+      eatery (dict): A valid json dictionary from Cornell Dining that contains eatery information
+  """
   dining_items = {'items': []}
   for item in eatery['diningItems']:
     dining_items['items'].append({
@@ -157,6 +226,13 @@ def parse_dining_items(eatery):
   return [dining_items]
 
 def parse_coordinates(eatery):
+  """Parses the coordinates of an eatery.
+
+  Returns a new CoordinateType that holds the geographic coordinates of an eatery.
+
+  Args:
+      eatery (dict): A valid json dictionary from Cornell Dining that contains eatery information
+  """
   latitude, longitude = 0.0, 0.0
   if 'coordinates' in eatery:
     latitude = eatery['coordinates']['latitude']
@@ -167,6 +243,13 @@ def parse_coordinates(eatery):
   )
 
 def parse_campus_area(eatery):
+  """Parses the common name location of an eatery.
+
+  Returns a new CampusAreaType that contains a description of an eatery
+
+  Args:
+      eatery (dict): A valid json dictionary from Cornell Dining that contains eatery information
+  """
   description, description_short = '', ''
   if 'campusArea' in eatery:
     description_short = eatery['campusArea']['descrshort']
@@ -175,6 +258,15 @@ def parse_campus_area(eatery):
   )
 
 def parse_static_eateries(static_json):
+  """Parses a static dining json dictionary.
+
+  Similar to the parse_eatery function except for static source. Uses parse_static_op_hours to
+  populate the operating_hours field in a CampusEateryType.
+
+  Args:
+      static_json (dict): A valid dictionary in the format of the dynamic Cornell Dining json for
+        static eateries
+  """
   for eatery in static_json['eateries']:
     new_eatery = CampusEateryType(
         about=eatery.get('about', ''),
@@ -211,12 +303,31 @@ def resolve_id(eatery, collegetown=False):
   return -1 * len(static_eateries)
 
 def parse_eatery_type(eatery):
+  """Parses the classification of an eatery.
+
+  Returns the type of an eatery (dining hall, cafe, etc).
+
+  Args:
+      eatery (dict): A valid json dictionary from Cornell Dining that contains eatery information
+  """
   try:
     return eatery['eateryTypes'][0]['descr']
   except Exception:
     return 'Unknown'
 
 def parse_static_op_hours(hours_list, dining_items, dates_closed):
+  """Parses the hours of a static eatery.
+
+  Returns a list with an OperatingHoursType, similar to parse_operating_hours, but includes
+  checking for dates the eatery is closed.
+
+  Args:
+      hours_list (list): A list containing the days a static eatery is open
+      dining_items (list): A list that holds a dictionary for the items an eatery serves and a flag
+       for healthy options
+      dates_closed (list): A list containg all the dates that an eatery is closed outside of its
+        weekly schedule
+  """
   weekdays = {}
   for hours in hours_list:
     if '-' in hours['weekday']:
@@ -287,10 +398,25 @@ def format_time(start_time, end_time, start_date, hr24=False, overnight=False):
   return [new_start, new_end]
 
 def get_trillium_menu():
+  """Gets the Trillium menu.
+
+  Returns the Trillium dining items (using parse_dining_items) from the static json source
+  for menus.
+  """
   static_json = requests.get(STATIC_MENUS_URL).json()
   return parse_dining_items(static_json['Trillium'][0])
 
 def merge_hours(eateries):
+   """Merges invalid events with valid events
+
+   Combines events with no menu and a start_time equal to the end_time of a previous event into
+   one event. This removes the effectively removes the events with no menus and only preserves
+   the end time of the invalid event.
+
+   Args:
+       eateries (dict): A dictionary containing information provided from Cornell Dining and filled
+         with CampusEateryTypes
+   """
   for eatery in eateries.values():
     for operating_hour in eatery.operating_hours:
       if len(operating_hour.events) <= 1:  # ignore hours that don't have multiple events
@@ -304,6 +430,13 @@ def merge_hours(eateries):
         base_event = event
 
 def parse_collegetown_eateries(collegetown_data):
+  """Parses Collegetown json dictionary.
+
+  Fills the collegetown_eateries list with objects of type CollegetownEateryType.
+
+  Args:
+      collegetown_data (dict): A valid json dictionary from Yelp that contains eatery information
+  """
   for eatery in collegetown_data:
     new_id = resolve_id(eatery, collegetown=True)
     new_eatery = CollegetownEateryType(
@@ -331,6 +464,14 @@ def parse_collegetown_eateries(collegetown_data):
     collegetown_eateries[new_eatery.id] = new_eatery
 
 def parse_collegetown_hours(eatery):
+  """Parses the hours of a Collegetown eatery.
+
+  Returns a list of CollegetownHoursType. Calls parse_collegetown_events to fill the events field
+  of CollegetownHoursType.
+
+  Args:
+      eatery (dict): A valid json dictionary from Yelp that contains eatery information
+  """
   hours_list = eatery.get('hours', [{}])[0].get('open', [])
   # gets open hours from first dictionary in hours, empty dict-list provided to mimic hours format
   new_operating_hours = []
@@ -347,6 +488,14 @@ def parse_collegetown_hours(eatery):
   return new_operating_hours
 
 def parse_collegetown_events(event_list, event_date):
+  """Parses the events in a Collegetown eatery.
+
+  Returns a list of CollegetownEventType.
+
+  Args:
+      event_list (list): Contains a list of times an eatery is open
+      event_date (string): a string representation of the date
+  """
   new_events = []
   for event in event_list:
     start, end = format_time(
