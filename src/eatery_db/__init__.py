9fd348e7c1717fa0b16b032d59131a48596cd3c4
Orko Sinha <37596785+orkosinha@users.noreply.github.com>
2019-12-01 13:32:20 -0500

    Finish database population (#156)

--- src/eatery_db/__init__.py
+++ src/eatery_db/__init__.py
@@ -1,5 +1,16 @@
 # flake8: noqa
 
-from .campus_eatery import parse_campus_eateries, parse_campus_hours, parse_menu_categories, parse_menu_items
+from .campus_eatery import (
+    parse_campus_eateries,
+    parse_campus_hours,
+    parse_menu_categories,
+    parse_menu_items,
+    parse_static_eateries,
+    parse_static_op_hours,
+)
 
-from .collegetown_eatery import parse_collegetown_eateries
+from .collegetown_eatery import parse_collegetown_eateries, parse_collegetown_hours
+
+from .common_eatery import parse_expanded_menu, parse_expanded_items, parse_expanded_choices
+
+from .swipes import export_data, parse_to_csv
