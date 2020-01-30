5b303be149522fb835a280c1c37f645e271e7fa6
meganle <meganle7@gmail.com>
2020-01-30 12:25:56 -0500

    Final fix to get campus and ctown cronjobs working

--- update_campus.sh
+++
@@ -1,1 +0,0 @@
-python -c 'from src.db import start_update; print("Running campus and swipe data update"); start_update(True, True, False)' > >> /var/log/cron.log
--- update_ctown.sh
+++
@@ -1,1 +0,0 @@
-python -c 'from src.db import start_update; print("Running collegetown update"); start_update(False, False, True)' >> /var/log/cron.log
