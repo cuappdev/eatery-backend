67097cf0e6161787f504af4f373e6ad9ff00e6ce
yuna878 <36527727+yuna878@users.noreply.github.com>
2020-10-21 15:03:27 +0900

    Delete old code
    Co-authored-by: asz33@cornell.edu <asz33@cornell.edu>

--- src/eatery/__init__.py
+++
@@ -1,6 +0,0 @@
-# flake8: noqa
-from .campus_eatery import parse_eatery
-
-from .collegetown_eatery import parse_collegetown_eateries
-
-from .static_eatery import parse_static_eateries, string_to_date_range
